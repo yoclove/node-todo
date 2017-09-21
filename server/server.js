4df4d2f6e67331e2d8290fd822748a7c64ed8ff8
Panzhe <799305728@qq.com>
2017-09-21 15:08:01 +0800

    mongoose-model

--- server/server.js
+++ server/server.js
@@ -1,6 +1,41 @@
 var mongoose = require('mongoose');
-var url = 'mongodb://root:799305728@ds062448.mlab.com:62448/todo';
 
-mongoose.connect(url, function(){
-	
+mongoose.Promise = global.Promise;
+var url = 'mongodb://root:799305728@ds062448.mlab.com:62448/todo';
+mongoose.connect(url, { useMongoClient: true, promiseLibrary: global.Promise });
+
+var Todo = mongoose.model('Todo',{
+	text: {
+		type: String	
+	},
+	completed: {
+		type: Boolean	
+	},
+	completedAt: {
+		type: Number
+	}
+});
+
+var newTodo = new Todo({
+	text: '这么麻烦呢'
+});
+
+
+newTodo.save().then(function(doc){
+	console.log(doc+'成功保存');
+}, function(err){
+	console.log('不能存储');
+})
+
+
+var otherTodo = new Todo({
+	text: '长安',
+	completed: true,
+	completedAt: 123
+});
+
+otherTodo.save().then(function(doc){
+	console.log(doc+'成功保存');
+}, function(err){
+	console.log('不能存储');
 })
